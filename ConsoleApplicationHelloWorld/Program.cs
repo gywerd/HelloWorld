a426fc43e599cf3932089d9b1e74671185596bc5
gywerd <31882858+gywerd@users.noreply.github.com>
2017-09-12 11:28:03 +0200

    Add files via upload

---
+++ ConsoleApplicationHelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x <= 1000; x++)
+            {
+                Console.WriteLine("Hello World " + x.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
